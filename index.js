9c86796fc8c25077221e1fda8f2065f106a255e4
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-21 13:17:58 +0300

    create control.js

--- index.js
+++ index.js
@@ -1,49 +1,36 @@
-function scuberGreetingForFeet(){
-  describe('index.js', function () {
-    describe('scuberGreetingForFeet()', function () {
-      it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-        expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
-      });
-  
-      // tests continue...
-    });
-  });
+function scuberGreetingForFeet(someNumber) {
+
+  if (someNumber <= 400){
+          return "This one is on me!";
+  }
+  else if (someNumber > 2000 && someNumber < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(someNumber > 2500){
+      return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
-  function addFive(someNumber) {
-    //Everything I want my function to do I put inside these curly braces
-    //In this example, let's say I want my function, addFive, to add 5 to
-    //any number I pass in (someNumber), but only IF the number is greater
-    //than zero:
-    let result
-    if (someNumber > 0) {
-      result = someNumber + 5;
-    }
-    //at the end, if I want my function to return something, I need to state it:
-    return result
+function ternaryCheckCity(city) {
+
+  if (city === "NYC") {
+      return "Ok, sounds good.";
+  }
+  else{
+      return "No go.";
+  }
+}
+
+
+function switchOnCharmFromTip(tip) {
+
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
   }
   
-  //once our function is declared, we can call addFive, passing in values 
-  //as arguments:
-  
-  addFive(10);
-  //=> 15
-  
-  addFive(20);
-  //=> 25
-  
-  addFive(-5);
-  //=> undefined
-  
-  addFive(addFive(5));
-  //=> 15!! In this case, the return value of addFive(5), 10, is passed in 
-  //as the argument to the outer addFive, returning 15
-  2 < 5 < 4
-// => true
-Number(true);
-// => 1
-
 }
-  
-    
